d1501926cdf2474e6c6e7faeac263e79f0efd44f
dachnic7384-bit <dachnic7384@gmail.com>
2026-01-15 00:57:56 +1100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,1 @@
--- Загрузчик MyRobloxScripts v1.0
--- GitHub: https://github.com/dachnic7384-bit/MyRobloxScripts
-
-print("🐝 Загружаю Bee Swarm Simulator Script...")
-loadstring(game:HttpGet("https://raw.githubusercontent.com/dachnic7384-bit/MyRobloxScripts/main/main.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dachnic7384-bit/MyRobloxScripts/main/gui.lua"))()
