58f0fb67419f56c932218805fbac5f40eb66b5b5
dachnic7384-bit <dachnic7384@gmail.com>
2026-01-14 21:25:15 +1100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,5 @@
 -- Загрузчик MyRobloxScripts v1.0
 -- GitHub: https://github.com/dachnic7384-bit/MyRobloxScripts
 
-print("📥 Загружаю MyRobloxScripts...")
+print("📥 Загрузка MyRobloxScripts...")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/dachnic7384-bit/MyRobloxScripts/main/main.lua"))()
